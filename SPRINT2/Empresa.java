37cfc01f00a24a287e796f37bbd55b5d452ce681
mceamy <amymontenegro.c@gmail.com>
2022-08-28 17:57:47 -0500

    Creacion de clase Empresa

---
+++ SPRINT2/Empresa.java
@@ -0,0 +1,25 @@
+public class Empresa {
+
+    private long id;
+    private String nit;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+//CONSTRUCTOR//
+    public Empresa(long id, String nit, String nombre, String direccion, String telefono) {
+        this.id = id;
+        this.nit = nit;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+//GETTERS Y SETTERS//
+    
+
+
+
+
+
+}
