d335f6fe998ab2d1dcd3bc98857d9b628b22c79a
betfie1@gmail.com <betfie1@gmail.com>
2022-08-28 19:43:20 -0500

    getters y setters clase Empleado

--- SPRINT2/Empleado.java
+++ SPRINT2/Empleado.java
@@ -16,6 +16,47 @@ public class Empleado {
     }
 
 //GETTERS Y SETTERS//
+    //idempleado
+    public void setIdEmpleado(long idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
 
+    public long getIdEmpleado() {
+        return idEmpleado;
+    }
 
+    //nombreEmpleado
+    public void setNombreEmpleado(String nombreEmpleado) {
+        this.nombreEmpleado = nombreEmpleado;
+    }
+
+    public String getNombreEmpleado() {
+        return nombreEmpleado;
+    }
+
+    //correo
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    //nombreEmpresa
+    public void setNombreEmpresa(String nombreEmpresa) {
+        this.nombreEmpresa = nombreEmpresa;
+    }
+
+    public String getNombreEmpresa() {
+        return nombreEmpresa;
+    }
+
+    public void setRolEmpleado(String rolEmpleado) {
+        this.rolEmpleado = rolEmpleado;
+    }
+
+    public String getRolEmpleado() {
+        return rolEmpleado;
+    }
 }
