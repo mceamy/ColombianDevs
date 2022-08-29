8dff242e5a07460c7fcbb7d6f172378cb3de3855
Santiago <sgi1604@hotmail.com>
2022-08-28 20:29:42 -0500

    Prueba de instancia de clases empleado y empresa

--- SPRINT2/Sprint2-ColombianDevs/src/Main.java
+++ SPRINT2/Sprint2-ColombianDevs/src/Main.java
@@ -1,11 +1,49 @@
 public class Main {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Pruebas de instancia de clases");
+
+        //Creación de empresa//
         Empresa empresa = new Empresa(12563, "105.589.562", "VyR SAS", "Av Poblado",  "8442552");
+        String resumenEmpresa = (empresa.getNombreEmpresa()+ "-" + empresa.getDireccion() + "-" + empresa.getTelefono()
+                + "-" + empresa.getNit());
+        System.out.println(resumenEmpresa);
+
+        //Modificación de empresa//
+        empresa.setIdEmpresa(14859);
         empresa.setNombreEmpresa("Lugon");
         empresa.setDireccion("Cra 65a 99-158");
         empresa.setTelefono("3218888895");
         empresa.setNit("103.526.896");
+        String resumenEmpresaCambios = (empresa.getNombreEmpresa()+ "-" + empresa.getDireccion() + "-" + empresa.getTelefono()
+                + "-" + empresa.getNit());
+        System.out.println(resumenEmpresaCambios);
+
+        //Creación de empleado//
+        Empleado empleado = new Empleado(12568,"Juan Camilo","ghjt@gmail.com","Lugon","Ingeniero");
+        String resumenEmpleado = (empleado.getNombreEmpleado() + "-" + empleado.getCorreo() + "-" + empleado.getRolEmpleado()
+                + "-" + empleado.getNombreEmpresa());
+        System.out.println(resumenEmpleado);
+
+
+        //Modificación de empleado//
+        empleado.setIdEmpleado(1253);
+        empleado.setNombreEmpleado("Santiago Gonzalez");
+        empleado.setRolEmpleado("Supervisor");
+        empleado.setCorreo("asd@hotmail.com");
+        empleado.setNombreEmpresa("VyR");
+        String resumenEmpleadoCambios = (empleado.getNombreEmpleado() + "-" + empleado.getCorreo() + "-" + empleado.getRolEmpleado()
+                + "-" + empleado.getNombreEmpresa());
+        System.out.println(resumenEmpleadoCambios);
+
+
+        /*MovimientoDinero Movimiento = new MovimientoDinero();
+        Movimiento.setMonto(-155000);
+        Movimiento.setConcepto("Recibo de Luz");
+        Movimiento.setEmpleado("Santiago Gonzalez");
+
+        String resumenMovimiento = (Movimiento.getMonto() + "-" + Movimiento.getConcepto() + "-"
+                + Movimiento.getEmpleado());
+        */
 
     }
 }
