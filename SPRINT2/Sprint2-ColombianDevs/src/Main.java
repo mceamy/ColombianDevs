a43ab37770929953418467d3855c56739c9a2d22
Santiago <sgi1604@hotmail.com>
2022-08-28 20:10:10 -0500

    Creación de java class main para pruebas de instancia

---
+++ SPRINT2/Sprint2-ColombianDevs/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Empresa empresa = new Empresa(12563, "105.589.562", "VyR SAS", "Av Poblado",  "8442552");
+        empresa.setNombreEmpresa("Lugon");
+        empresa.setDireccion("Cra 65a 99-158");
+        empresa.setTelefono("3218888895");
+        empresa.setNit("103.526.896");
+
+    }
+}
