32a26ac3ef4c0ad81c0de20620b71affec962fa1
Santiago <sgi1604@hotmail.com>
2022-09-25 13:15:35 -0500

    Inicialización controladores

---
+++ Sprint 3/MinTic/src/main/java/com/Proyecto/MinTic/MinTicApplication.java
@@ -0,0 +1,14 @@
+package com.Proyecto.MinTic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinTicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinTicApplication.class, args);
+		System.out.println("Hola Mundo");
+	}
+
+}
